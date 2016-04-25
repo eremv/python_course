df80ab2deb760238bf3a70b3eb55af453d7a0c1f
Vadim Eremichev <ve@anahoret.com>
2016-04-25 17:01:04 +0300

    add graph search

--- 1.6.1.py
+++ 1.6.1.py
@@ -1,5 +1,5 @@
-
 rules={}
+requests=[]
 
 def read():
     n = int(input())
@@ -10,5 +10,39 @@ def read():
             rules[str.rstrip(r[0])] = r[1].split()
         else:
             rules[r[0]] = []
+    n = int(input())
+    global requests
+    for i in range(n):
+        st = input().split()
+        requests.append(st)
 
+def is_parent(parent, child):
+    print('Looking for ', parent,' is parent of ',child)
+    print('Strict parents of ',child,' are : ', rules[child])
+    if rules[child]==[]: 
+        print('empty array for rules[child] = ', rules[child])
+        return 'No'
+    if parent == child: 
+        print('parent is child')
+        return 'Yes'
+    elif parent in rules[child]:
+        print('parent', parent, ' is in ', rules[child])
+        return 'Yes'
+    else:
+        print('enter the recursia')
+        for p in rules[child]:
+            print(' check is parent for child = ', p , ' ,parent = ', parent)
+            is_parent(parent, p)
 
+def process():
+    read()
+    for r in requests:
+        print( is_parent(r[0], r[1]) )
+
+process()
+3
+A
+B : A
+C : B
+1
+A C
